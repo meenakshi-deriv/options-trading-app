d53eab76b3b483ee492e585b01eca79357645c75
meenakshi-deriv <meenakshi@regentmarkets.com>
2024-12-10 14:54:57 +0400

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
 import Trading from './components/Trading';
